76dd1daba1ae937daafd71bd5209526866cce9f1
Aurora Dawn <131844170+StellarWitch7@users.noreply.github.com>
2023-05-11 15:25:47 -0400

    Added more javadoc

--- src/main/java/com/github/stellarwitch7/earthguard/util/IExplosiveItem.java
+++ src/main/java/com/github/stellarwitch7/earthguard/util/IExplosiveItem.java
@@ -8,11 +8,16 @@ public interface IExplosiveItem {
 	float getExplosiveStrength();
 	Explosion.DestructionType getDestructionType();
 	/**
-	 *This method overrides a method from the Minecraft Item class.
+	 *Overrides a method from the Minecraft Item class.
 	 * To make the item explode when it is destroyed, call IExplosiveItem.super.explode(entity)
 	 * @param entity
 	 */
 	void onItemEntityDestroyed(ItemEntity entity);
+	/**
+	 *Creates an explosion at the location of the item. Call it from onItemEntityDestroyed
+	 * to make the item entity explode when it is destroyed.
+	 * @param entity
+	 */
 	default void explode(ItemEntity entity) {
 		World world = entity.getWorld();
 		world.createExplosion(entity,
