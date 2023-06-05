30d3312556184bc4ef87e802e6ae1fbf48606b5f
Aurora Dawn <131844170+StellarWitch7@users.noreply.github.com>
2023-06-05 03:51:26 -0400

    Update ValkatrosEntity.java

--- src/main/java/com/github/stellarwitch7/earthguard/entity/boss/ValkatrosEntity.java
+++ src/main/java/com/github/stellarwitch7/earthguard/entity/boss/ValkatrosEntity.java
@@ -2,6 +2,7 @@ package com.github.stellarwitch7.earthguard.entity.boss;
 
 import com.github.stellarwitch7.earthguard.EarthguardMod;
 import com.github.stellarwitch7.earthguard.util.BossPhase;
+import com.github.stellarwitch7.earthguard.util.SpecialValues;
 import com.mojang.serialization.DataResult;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
@@ -16,9 +17,14 @@ import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.nbt.NbtCompound;
 import net.minecraft.nbt.NbtElement;
 import net.minecraft.nbt.NbtOps;
+import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
 import org.jetbrains.annotations.Nullable;
 import software.bernie.geckolib3.core.IAnimatable;
+import software.bernie.geckolib3.core.PlayState;
+import software.bernie.geckolib3.core.builder.AnimationBuilder;
+import software.bernie.geckolib3.core.controller.AnimationController;
+import software.bernie.geckolib3.core.event.predicate.AnimationEvent;
 import software.bernie.geckolib3.core.manager.AnimationData;
 import software.bernie.geckolib3.core.manager.AnimationFactory;
 
@@ -33,15 +39,26 @@ Lightning, explosive projectiles
 */
 
 public class ValkatrosEntity extends HostileEntity implements RangedAttackMob, IAnimatable {
+	private final double dashDistance = 20.0d;
+	private final int dashLength = (int)(SpecialValues.TICK_SECOND * 2.5);
+	private final float dashDamage = 10.0f;
+	private final int lightningDelay = SpecialValues.TICK_SECOND * 3;
 	private AnimationFactory factory = new AnimationFactory(this);
 	private BossPhase bossPhase = BossPhase.ONE;
-	private boolean isDashing = false;
 	private boolean approachTarget = false;
-	private double dashDistance = 20.0d;
+	private boolean transitioning = false;
+	private boolean isDashing = false;
+	private int dashTimer = 0;
+	private LivingEntity dashTarget;
+	private boolean callingLightning = false;
+	private int lightningTimer = 0;
+	private Vec3d lightningTargetPos;
 	
 	protected ValkatrosEntity(EntityType<? extends HostileEntity> entityType, World world) {
 		super(entityType, world);
 	}
+	
+	
 	
 	@Override
 	protected void initGoals() {
@@ -77,8 +94,25 @@ public class ValkatrosEntity extends HostileEntity implements RangedAttackMob, I
 	}
 	
 	private void dashAttack(LivingEntity target) {
-		this.setVelocity(target.getPos().multiply(2));
+		dashTarget = target;
+		dashTimer = dashLength;
 		isDashing = true;
+	}
+	
+	private boolean dashLogic(LivingEntity target) {
+		if (dashTimer > 0) {
+			dashTimer--;
+		} else {
+			return false;
+		}
+		
+		if (this.collidesWith(target)) {
+			target.damage(DamageSource.CRAMMING, dashDamage);
+			return false;
+		}
+		
+		this.setVelocity(target.getPos().multiply(2));
+		return true;
 	}
 	
 	private void launchChaosProjectile(LivingEntity target) {
@@ -86,10 +120,43 @@ public class ValkatrosEntity extends HostileEntity implements RangedAttackMob, I
 	}
 	
 	private void callLightning(LivingEntity target) {
-		World world = this.getWorld();
-		LightningEntity lightningBolt = new LightningEntity(EntityType.LIGHTNING_BOLT, world);
-		lightningBolt.setPosition(target.getPos());
-		world.spawnEntity(lightningBolt);
+		/*
+		Add logic here to summon an entity at the position of the target
+		that will simply be a marker to let player's know where the next strike
+		will be.
+		*/
+		
+		lightningTargetPos = target.getPos();
+		lightningTimer = lightningDelay;
+		callingLightning = true;
+	}
+	
+	private boolean lightningLogic(Vec3d targetPos) {
+		if (lightningTimer > 0) {
+			lightningTimer--;
+		} else {
+			World world = this.getWorld();
+			LightningEntity lightningBolt = new LightningEntity(EntityType.LIGHTNING_BOLT, world);
+			lightningBolt.setPosition(targetPos);
+			world.spawnEntity(lightningBolt);
+			
+			return false;
+		}
+		
+		return true;
+	}
+	
+	@Override
+	public void tick() {
+		super.tick();
+		
+		if (isDashing) {
+			isDashing = dashLogic(dashTarget);
+		}
+		
+		if (callingLightning) {
+			callingLightning = lightningLogic(lightningTargetPos);
+		}
 	}
 	
 	@Override
@@ -128,9 +195,34 @@ public class ValkatrosEntity extends HostileEntity implements RangedAttackMob, I
 		return false;
 	}
 	
+	private PlayState predicate(AnimationEvent event) {
+		var animations = new AnimationBuilder();
+		
+		if (isDashing && bossPhase == BossPhase.ONE) {
+			animations.addAnimation("animation.valkatros.dash");
+			event.getController().setAnimation(animations);
+			return PlayState.CONTINUE;
+		}
+		
+		//Idle
+		if (bossPhase == BossPhase.TWO) {
+			if (transitioning) {
+				animations.addAnimation("animation.valkatros.transition", true);
+			} else {
+				animations.addAnimation("animation.valkatros.phase_two_idle", true);
+			}
+		} else if (bossPhase == BossPhase.ONE) {
+			animations.addAnimation("animationg.valkatros.phase_one_idle", true);
+		}
+		
+		event.getController().setAnimation(animations);
+		return PlayState.CONTINUE;
+	}
+	
 	@Override
 	public void registerControllers(AnimationData animationData) {
-	
+		animationData.addAnimationController(new AnimationController(this, "controller",
+				0, this::predicate));
 	}
 	
 	@Override
